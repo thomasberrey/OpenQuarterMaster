a8238f867d5a0de787b372b5c16282bd1760df55
EC2 Default User <ec2-user@ip-172-31-7-237.us-west-1.compute.internal>
2022-06-14 03:51:58 +0000

    Added SSO keycloak files

--- quartermaster_deploy.sh
+++ quartermaster_deploy.sh
@@ -4,9 +4,14 @@ oc new-project quartermaster
 
 pushd yaml/openshift
 ./install.sh
+popd
 
 oc patch servicemeshmemberroll/default -n istio-system --type=merge -p '{"spec": {"members": ["quartermaster"]}}'
 
 oc patch route/istio-ingressgateway -n istio-system --type=merge -p '{"spec": {"tls": {"termination": "edge"}}}'
 oc patch route/istio-ingressgateway -n istio-system --type=merge -p '{"spec": {"tls": {"insecureEdgeTerminationPolicy": "Redirect"}}}'
+
+oc apply -f sso_keycloak.yml
+oc apply -f sso_keycloakrealm.yml
+
 
